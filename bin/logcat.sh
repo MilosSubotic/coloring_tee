badeedd708d00163fe1a866b87c5dffa186a2470
Milos Subotic <milos.subotic.sm@gmail.com>
2013-05-07 23:50:06 -0400

    Added pipefail to scripts.

--- bin/logcat.sh
+++ bin/logcat.sh
@@ -7,6 +7,8 @@ LOG_DIR="$PWD"
 # Uncomment this line if you need to clean logcat before logging to files.
 #adb logcat -c
 
+set -o pipefail
+
 adb logcat -v time *:V 2>&1 | coloring_tee \
 	--color-schemes=logcat --html="$LOG_DIR/log.html" "$LOG_DIR/log.logcat"
 	
